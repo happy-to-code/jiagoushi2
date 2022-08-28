cbea54930929d9695b8094f492ab51018f5f38be
happy-to-code <yida8080@gmail.comc>
2022-08-28 18:09:09 +0800

    update gitignore

--- hailtaxi-nacos-sentinel-parent/hailtaxi-driver-ns/src/main/java/com/yueda/driver/controller/DriverController.java
+++ hailtaxi-nacos-sentinel-parent/hailtaxi-driver-ns/src/main/java/com/yueda/driver/controller/DriverController.java
@@ -1,5 +1,9 @@
 package com.yueda.driver.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.system.SystemBlockException;
+import com.sun.deploy.security.BlockedException;
 import com.yueda.driver.model.Driver;
 import com.yueda.driver.service.DriverService;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +19,7 @@ import java.util.Enumeration;
 @RequestMapping(value = "/driver")
 @Slf4j
 @RefreshScope
+@SentinelResource(defaultFallback = "defaultExHandler")
 public class DriverController {
 	
 	@Autowired
@@ -22,16 +27,51 @@ public class DriverController {
 	@Value("${app.name}")
 	private String name;
 	
+	public Driver defaultExHandler(Throwable e) {
+		Driver driver = new Driver();
+		driver.setName("defaultExHandler,系统繁忙,稍后再试");
+		return driver;
+	}
 	
 	/****
 	 * 司机信息
 	 */
-	@GetMapping(value = "/info/{id}")
-	public Driver info(@PathVariable(value = "id") String id) {
+	// @SentinelResource(value = "info", fallback = "exHandler")
+	@SentinelResource(value = "info")
+	@GetMapping("/info/{id}")
+	public Driver info(@PathVariable(value = "id") String id) throws BlockException {
 		Driver driver = driverService.findById(id);
 		if (driver == null) {
+			// throw new SystemBlockException("司机id:" + id + " 不存在",null);
 			throw new RuntimeException("司机id:" + id + " 不存在");
 		}
+		return driver;
+	}
+	
+	/**
+	 * info资源出现任何类型异常后的降级处理  法参数可以添加一个Throwable 类型的参数，也可不添加
+	 * @param id
+	 * @param e
+	 * @return
+	 */
+	public Driver exHandler(String id, Throwable e) {
+		Driver driver = new Driver();
+		driver.setId(id);
+		driver.setName("系统繁忙,稍后再试");
+		return driver;
+	}
+	
+	/**
+	 * 出现错误走降级方法
+	 *
+	 * @param id
+	 * @param e
+	 * @return
+	 */
+	public Driver blockExceptionHandler(String id, BlockException e) {
+		Driver driver = new Driver();
+		driver.setId(id);
+		driver.setName("系统繁忙,稍后再试");
 		return driver;
 	}
 	
