6db8db9b9ccd2cabdcba96877a573b338a15379f
lvdekai <lvdekai@ycimedia.com>
2022-11-14 09:49:50 +0800

    Class 获取类路径

---
+++ a-yida-demo/src/main/java/com/yida/pojo/Person.java
@@ -0,0 +1,21 @@
+package com.yida.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Type: Person.java
+ * @Desc:
+ * @author: zhangyifei
+ * @Date: 2022/11/14 9:44
+ * @Version:
+ */
+@Data
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 8185480882553688096L;
+
+    private String name;
+    private Integer age;
+}
