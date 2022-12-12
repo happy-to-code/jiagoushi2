90284ad6bba19ea03c069ce4d57b835b298d4e2f
happy-to-code <yida8080@gmail.comc>
2022-12-12 23:19:23 +0800

    dead_lock

--- a-yida-demo/src/main/java/com/yida/controller/MyTestController.java
+++ a-yida-demo/src/main/java/com/yida/controller/MyTestController.java
@@ -2,6 +2,7 @@ package com.yida.controller;
 
 import com.yida.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyTestController {
     @Autowired
     private TestService testService;
+    
+    @Value("${server.port}")
+    private Integer port;
+    
+    @Value("${abc.d}")
+    private String d;
 
     @GetMapping("get")
     public String getTest() {
+        System.out.println("port = " + port);
+        System.out.println("d =---------> " + d);
         return testService.testGet("", 1);
     }
 }
