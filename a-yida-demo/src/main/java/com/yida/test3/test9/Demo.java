eb21fd31ca053e93c7af5b7db687b2d1470d344f
happy-to-code <yida8080@gmail.comc>
2023-05-29 10:23:42 +0800

    2023-5-29

--- a-yida-demo/src/main/java/com/yida/test3/test9/Demo.java
+++ a-yida-demo/src/main/java/com/yida/test3/test9/Demo.java
@@ -12,5 +12,7 @@ public class Demo {
 		String url = "http://139.196.1.239/JianzhouSMSWSServer/http/sendBatchMessage?account=sdk_guquan&password=sdk_gqjy630&msgText=【上海股权托管交易中心】您的爱私基摩X家用户身份已认证，请您通过上海股权托管交易中心微信公众号->我要报价登陆查看&destmobile=17625341118";
 		String post = HttpUtil.post(url, "");
 		System.out.println("post = " + post);
+		
+		
 	}
 }
